df196f48978a5bf649f26e8b803423f71aa5717b
Brent Laster <bcl@nclasters.org>
2022-07-19 22:42:18 -0400

    update setup script

--- setup-no-vm.sh
+++ setup-no-vm.sh
@@ -2,13 +2,12 @@
 # assumes existing kubernetes cluster
 # docker registry needs to be available at localhost:5000
 # puts command line interface in /usr/local/bin
+# get the appropriate command line interface from https://github.com/tektoncd/cli/releases/download/v0.17.1
 
 kubectl create ns tekpipe
 kubectl create ns roar
 kubectl create secret -n tekpipe docker-registry regcreds --docker-server=http://localhost:5000/v2 --docker-username=*** --docker-password=***
-kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
-curl -LO https://github.com/tektoncd/cli/releases/download/v0.17.1/tkn_0.17.1_Linux_x86_64.tar.gz
-sudo tar xvzf tkn_0.17.1_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/previous/v0.27.1/release.yaml
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/tekton-dashboard-release.yaml
 
 kubectl apply -f https://storage.googleapis.com/tekton-releases/triggers/previous/v0.14.2/release.yaml
